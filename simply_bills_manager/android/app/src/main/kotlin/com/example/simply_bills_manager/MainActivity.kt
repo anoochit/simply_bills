dab8e05bf5db947d40907a6b8fbb743257377751
Anuchit Chalothorn <anoochit@gmail.com>
2024-08-04 21:18:58 +0700

    add more front-end apps

---
+++ simply_bills_manager/android/app/src/main/kotlin/com/example/simply_bills_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simply_bills_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ simply_bills_officer/android/app/src/main/kotlin/com/example/simply_bills_officer/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.simply_bills_officer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
